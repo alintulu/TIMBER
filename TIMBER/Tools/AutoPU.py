a0c60be883105d2d0d7081b91959dc7abc48d013
alintulu <adelina.eleonora.lintuluoto@cern.ch>
2021-06-07 16:11:52 +0200

    Amend AutoPU

--- TIMBER/Tools/AutoPU.py
+++ TIMBER/Tools/AutoPU.py
@@ -13,10 +13,11 @@ def AutoPU(a, era):
     ftemplate = ROOT.TFile.Open(TIMBERPATH+'/TIMBER/data/Pileup/pileup_%s.root'%era)
     htemplate = ftemplate.Get('pileup')
     binning = ('autoPU','autoPU', htemplate.GetNbinsX(), htemplate.GetXaxis().GetXmin(), htemplate.GetXaxis().GetXmax())
-    autoPU = a.DataFrame.Histo1D(binning,"Pileup_nTrueInt").GetValue()
+    autoPU_hist = a.DataFrame.Histo1D(binning,"Pileup_nTrueInt")
+    autoPU = autoPU_hist.GetValue()
     ROOT.gROOT.cd()
     ROOT.gDirectory.Add(autoPU)
-    c_PU = Correction('pileup','TIMBER/Framework/src/Pileup_weight.cc',['"%s"'%era], corrtype="weight")
+    c_PU = Correction('pileup','TIMBER/Framework/src/Pileup_weight.cc',["%s"%era], corrtype="weight")
     a.AddCorrection(c_PU)
     ftemplate.Close()
     return a
